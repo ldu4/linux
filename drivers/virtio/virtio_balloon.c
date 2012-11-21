83bb61967444e22100f7c6e2a5f79ffa85b9e981
Michal Hocko <mhocko@suse.cz>
2012-11-21 16:42:02 +0100

    virtio_balloon-introduce-migration-primitives-to-balloon-pages-fix-fix-fix mismerge fix
    %u got back to %zu while while reverting
    %(4f2ac8495ba0477d8c3208de96dae7d1db6c2d49) obsolete version of
    virtio_balloon: introduce migration primitives to balloon pages
    
    Signed-off-by: Michal Hocko <mhocko@suse.cz>

--- drivers/virtio/virtio_balloon.c
+++ drivers/virtio/virtio_balloon.c
@@ -141,7 +141,7 @@ static void fill_balloon(struct virtio_balloon *vb, size_t num)
 		if (!page) {
 			if (printk_ratelimit())
 				dev_printk(KERN_INFO, &vb->vdev->dev,
-					   "Out of puff! Can't get %zu pages\n",
+					   "Out of puff! Can't get %u pages\n",
 					    VIRTIO_BALLOON_PAGES_PER_PAGE);
 			/* Sleep for at least 1/5 of a second before retry. */
 			msleep(200);
