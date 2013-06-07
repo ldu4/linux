3153be11486f1b79484be93561f8abe41a9d2adc
Michal Hocko <mhocko@suse.cz>
2013-06-07 16:44:21 +0200

    Reverted "vmscan: also shrink slab in memcg pressure"
    This reverts b5e47aad1e05b1b84dcb6dda3ea552cceae11836
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- include/linux/shrinker.h
+++ include/linux/shrinker.h
@@ -22,9 +22,6 @@ struct shrink_control {
 	nodemask_t nodes_to_scan;
 	/* current node being shrunk (for NUMA aware shrinkers) */
 	int nid;
-
-	/* reclaim from this memcg only (if not NULL) */
-	struct mem_cgroup *target_mem_cgroup;
 };
 
 /*
@@ -78,8 +75,7 @@ struct shrinker {
 #define DEFAULT_SEEKS 2 /* A good number if you don't know better. */
 
 /* Flags */
-#define SHRINKER_NUMA_AWARE	(1 << 0)
-#define SHRINKER_MEMCG_AWARE	(1 << 1)
+#define SHRINKER_NUMA_AWARE (1 << 0)
 
 extern int register_shrinker(struct shrinker *);
 extern void unregister_shrinker(struct shrinker *);
