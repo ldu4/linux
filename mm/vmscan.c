493b779f0d496fe5cdbbd3b264bba61935a650c0
Michal Hocko <mhocko@suse.cz>
2013-06-07 16:44:13 +0200

    Reverted "vmscan: take at least one pass with shrinkers"
    This reverts 9a7f23be7a31f08379e41803642c6bf1270a6919
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -297,34 +297,21 @@ shrink_slab_node(struct shrink_control *shrinkctl, struct shrinker *shrinker,
 				nr_pages_scanned, lru_pages,
 				max_pass, delta, total_scan);
 
-	do {
+	while (total_scan >= batch_size) {
 		long ret;
-		unsigned long nr_to_scan = min(batch_size, total_scan);
-		struct mem_cgroup *memcg = shrinkctl->target_mem_cgroup;
 
-		/*
-		 * Differentiate between "few objects" and "no objects"
-		 * as returned by the count step.
-		 */
-		if (!total_scan)
-			break;
-
-		if ((total_scan < batch_size) &&
-		   !(memcg && memcg_kmem_is_active(memcg)))
-			break;
-
-		shrinkctl->nr_to_scan = nr_to_scan;
+		shrinkctl->nr_to_scan = batch_size;
 		ret = shrinker->scan_objects(shrinker, shrinkctl);
 
 		if (ret == -1)
 			break;
 		freed += ret;
 
-		count_vm_events(SLABS_SCANNED, nr_to_scan);
-		total_scan -= nr_to_scan;
+		count_vm_events(SLABS_SCANNED, batch_size);
+		total_scan -= batch_size;
 
 		cond_resched();
-	} while (total_scan >= batch_size);
+	}
 
 	/*
 	 * move the unused scan count back into the shrinker in a
