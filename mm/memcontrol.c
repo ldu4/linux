aa5afd2c38efea4ea45e9df7cbac9027adb49648
Michal Hocko <mhocko@suse.cz>
2014-07-16 11:22:01 +0200

    Revert "memcg: deprecate memory.force_empty knob"
    This reverts commit 34cddd0d517b37b8e58a03ffc353bfdaadab4daf.
    
    CFTYPE_INSANE will get deprecated and kbobs for the default cgroups
    hierarchy are having a separate list of files now so this is not needed
    anymore.
    
    Signed-off-by: Michal Hocko <mhocko@suse.cz>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4086,10 +4086,6 @@ static ssize_t mem_cgroup_force_empty_write(struct kernfs_open_file *of,
 
 	if (mem_cgroup_is_root(memcg))
 		return -EINVAL;
-	pr_info("%s (%d): memory.force_empty is deprecated and will be removed.",
-			current->comm, task_pid_nr(current));
-	pr_cont(" Let us know if you know if it needed in your usecase at");
-	pr_cont(" linux-mm@kvack.org\n");
 	return mem_cgroup_force_empty(memcg) ?: nbytes;
 }
 
@@ -5266,7 +5262,6 @@ static struct cftype mem_cgroup_files[] = {
 	},
 	{
 		.name = "force_empty",
-		.flags = CFTYPE_INSANE,
 		.write = mem_cgroup_force_empty_write,
 	},
 	{
