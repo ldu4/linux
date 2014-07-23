22f32cf2e27603e8e9cebc951ab2e4cc44af9924
Michal Hocko <mhocko@suse.cz>
2014-07-23 09:51:53 +0200

    Reverted "mm: memcontrol: rewrite uncharge API fix - double migration"
    This reverts c7c550f96043f3c40da6fb6de3c5ae37cc655db1
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6608,10 +6608,7 @@ void mem_cgroup_migrate(struct page *oldpage, struct page *newpage,
 	if (!PageCgroupUsed(pc))
 		return;
 
-	/* Already migrated */
-	if (!(pc->flags & PCG_MEM))
-		return;
-
+	VM_BUG_ON_PAGE(!(pc->flags & PCG_MEM), oldpage);
 	VM_BUG_ON_PAGE(do_swap_account && !(pc->flags & PCG_MEMSW), oldpage);
 	pc->flags &= ~(PCG_MEM | PCG_MEMSW);
 
