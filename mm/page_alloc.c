f10dd72c33d244f1e1f1dbc2191666f6246fd484
Michal Hocko <mhocko@suse.cz>
2015-01-08 09:23:25 +0100

    Reverted "mm-page_allocc-__alloc_pages_nodemask-dont-alter-arg-gfp_mask-fix"
    This reverts bfa2d0e45fa54cdaf7245b0b1910fa5e7ed7eec3
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2867,7 +2867,6 @@ __alloc_pages_nodemask(gfp_t gfp_mask, unsigned int order,
 	unsigned int cpuset_mems_cookie;
 	int alloc_flags = ALLOC_WMARK_LOW|ALLOC_CPUSET|ALLOC_FAIR;
 	int classzone_idx;
-	gfp_t mask;
 
 	gfp_mask &= gfp_allowed_mask;
 
@@ -2901,24 +2900,23 @@ retry_cpuset:
 	classzone_idx = zonelist_zone_idx(preferred_zoneref);
 
 	/* First allocation attempt */
-	mask = gfp_mask|__GFP_HARDWALL;
-	page = get_page_from_freelist(mask, nodemask, order, zonelist,
-			high_zoneidx, alloc_flags, preferred_zone,
-			classzone_idx, migratetype);
+	page = get_page_from_freelist(gfp_mask|__GFP_HARDWALL, nodemask, order,
+			zonelist, high_zoneidx, alloc_flags,
+			preferred_zone, classzone_idx, migratetype);
 	if (unlikely(!page)) {
 		/*
 		 * Runtime PM, block IO and its error handling path
 		 * can deadlock because I/O on the device might not
 		 * complete.
 		 */
-		mask = memalloc_noio_flags(gfp_mask);
+		gfp_t mask = memalloc_noio_flags(gfp_mask);
 
 		page = __alloc_pages_slowpath(mask, order,
 				zonelist, high_zoneidx, nodemask,
 				preferred_zone, classzone_idx, migratetype);
 	}
 
-	trace_mm_page_alloc(page, order, mask, migratetype);
+	trace_mm_page_alloc(page, order, gfp_mask, migratetype);
 
 out:
 	/*
