58a668efc45e239a590456f0c03791f182aec3b3
Michal Hocko <mhocko@suse.com>
2015-07-30 10:41:25 +0200

    Reverted "mm-mlock-introduce-vm_lockonfault-and-add-mlock-flags-to-enable-it-v4-checkpatch-fixes"
    This reverts 517f777472043c8e9d76706f0c64bb0ba3f50e95
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/rmap.c
+++ mm/rmap.c
@@ -849,8 +849,7 @@ static int page_referenced_one(struct page *page, struct vm_area_struct *vma,
 
 		if (vma->vm_flags & (VM_LOCKED | VM_LOCKONFAULT)) {
 			spin_unlock(ptl);
-			pra->vm_flags |= (vma->vm_flags &
-					  (VM_LOCKED | VM_LOCKONFAULT));
+			pra->vm_flags |= (vma->vm_flags & (VM_LOCKED | VM_LOCKONFAULT));
 			return SWAP_FAIL; /* To break the loop */
 		}
 
@@ -880,8 +879,7 @@ static int page_referenced_one(struct page *page, struct vm_area_struct *vma,
 
 		if (vma->vm_flags & (VM_LOCKED | VM_LOCKONFAULT)) {
 			pte_unmap_unlock(pte, ptl);
-			pra->vm_flags |= (vma->vm_flags &
-					  (VM_LOCKED | VM_LOCKONFAULT));
+			pra->vm_flags |= (vma->vm_flags & (VM_LOCKED | VM_LOCKONFAULT));
 			return SWAP_FAIL; /* To break the loop */
 		}
 
