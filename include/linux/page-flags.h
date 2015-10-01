f2b77bcb21fc965e64c7af72d0cb80c23efeaeaf
Michal Hocko <mhocko@suse.com>
2015-10-01 10:52:17 +0200

    Reverted "page-flags: define PG_locked behavior on compound pages"
    This reverts 6439d7575f389e9c61b94f5c2089a03c0c329df9
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -229,7 +229,7 @@ static inline int PageCompound(struct page *page);
 static inline int PageTail(struct page *page);
 static inline struct page *compound_head(struct page *page);
 
-__PAGEFLAG(Locked, locked, NO_TAIL)
+TESTPAGEFLAG(Locked, locked, ANY)
 PAGEFLAG(Error, error, ANY) TESTCLEARFLAG(Error, error, ANY)
 PAGEFLAG(Referenced, referenced, ANY) TESTCLEARFLAG(Referenced, referenced, ANY)
 	__SETPAGEFLAG(Referenced, referenced, ANY)
