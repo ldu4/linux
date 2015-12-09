35fa3ffc1b1adb5e816731bec0a71aacf5cb6f02
Michal Hocko <mhocko@suse.com>
2015-12-09 10:27:55 +0100

    Reverted "arm64-mm-support-arch_mmap_rnd_bits-v4"
    This reverts 95d1d5be1455d8b7f877bb8ca2ccae558f7c716c
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- arch/arm64/mm/mmap.c
+++ arch/arm64/mm/mmap.c
@@ -51,11 +51,9 @@ unsigned long arch_mmap_rnd(void)
 {
 	unsigned long rnd;
 
-ifdef CONFIG_COMPAT
 	if (test_thread_flag(TIF_32BIT))
 		rnd = (unsigned long)get_random_int() % (1 << mmap_rnd_compat_bits);
 	else
-#endif
 		rnd = (unsigned long)get_random_int() % (1 << mmap_rnd_bits);
 	return rnd << PAGE_SHIFT;
 }
