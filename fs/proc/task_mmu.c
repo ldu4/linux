2e3ed46dc7a3566bdeb1707807e390c7e6a6f41f
Michal Hocko <mhocko@suse.com>
2016-01-28 09:14:40 +0100

    Revert "numa-fix-proc-pid-numa_maps-on-s390-fix"
    This reverts commit cd85cdc6c8a4eafc47a9e6e4ff55744f6361ee49.
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1514,7 +1514,6 @@ static int gather_pte_stats(pmd_t *pmd, unsigned long addr,
 	pte_t *pte;
 
 	ptl = pmd_trans_huge_lock(pmd, vma);
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	if (ptl) {
 		pte_t huge_pte = huge_ptep_get((pte_t *)pmd);
 		struct page *page;
@@ -1526,7 +1525,6 @@ static int gather_pte_stats(pmd_t *pmd, unsigned long addr,
 		spin_unlock(ptl);
 		return 0;
 	}
-#endif
 
 	if (pmd_trans_unstable(pmd))
 		return 0;
