4db500269c3c77783394985b7f2739425217d76e
Michal Hocko <mhocko@suse.com>
2016-04-07 11:30:35 +0200

    Reverted "zsmalloc: factor page chain functionality out"
    This reverts 1a1da0d27295376c9e4e1f832ce82f24a95ffcca
    
    Patch has been dropped from mmotm tree with reason: to-by-updated

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -981,9 +981,7 @@ static void init_zspage(struct size_class *class, struct page *first_page)
 	unsigned long off = 0;
 	struct page *page = first_page;
 
-	first_page->freelist = NULL;
-	INIT_LIST_HEAD(&first_page->lru);
-	set_zspage_inuse(first_page, 0);
+	VM_BUG_ON_PAGE(!is_first_page(first_page), first_page);
 
 	while (page) {
 		struct page *next_page;
@@ -1028,44 +1026,13 @@ static void init_zspage(struct size_class *class, struct page *first_page)
 	set_freeobj(first_page, 0);
 }
 
-static void create_page_chain(struct page *pages[], int nr_pages)
-{
-	int i;
-	struct page *page;
-	struct page *prev_page = NULL;
-	struct page *first_page = NULL;
-
-	for (i = 0; i < nr_pages; i++) {
-		page = pages[i];
-
-		INIT_LIST_HEAD(&page->lru);
-		if (i == 0) {
-			SetPagePrivate(page);
-			set_page_private(page, 0);
-			first_page = page;
-		}
-
-		if (i == 1)
-			set_page_private(first_page, (unsigned long)page);
-		if (i >= 1)
-			set_page_private(page, (unsigned long)first_page);
-		if (i >= 2)
-			list_add(&page->lru, &prev_page->lru);
-		if (i == nr_pages - 1)
-			SetPagePrivate2(page);
-
-		prev_page = page;
-	}
-}
-
 /*
  * Allocate a zspage for the given size class
  */
 static struct page *alloc_zspage(struct size_class *class, gfp_t flags)
 {
-	int i;
-	struct page *first_page = NULL;
-	struct page *pages[ZS_MAX_PAGES_PER_ZSPAGE];
+	int i, error;
+	struct page *first_page = NULL, *uninitialized_var(prev_page);
 
 	/*
 	 * Allocate individual pages and link them together as:
@@ -1078,22 +1045,42 @@ static struct page *alloc_zspage(struct size_class *class, gfp_t flags)
 	 * (i.e. no other sub-page has this flag set) and PG_private_2 to
 	 * identify the last page.
 	 */
+	error = -ENOMEM;
 	for (i = 0; i < class->pages_per_zspage; i++) {
 		struct page *page;
 
 		page = alloc_page(flags);
-		if (!page) {
-			while (--i >= 0)
-				__free_page(pages[i]);
-			return NULL;
-		}
+		if (!page)
+			goto cleanup;
 
-		pages[i] = page;
+		INIT_LIST_HEAD(&page->lru);
+		if (i == 0) {	/* first page */
+			page->freelist = NULL;
+			SetPagePrivate(page);
+			set_page_private(page, 0);
+			first_page = page;
+			set_zspage_inuse(page, 0);
+		}
+		if (i == 1)
+			set_page_private(first_page, (unsigned long)page);
+		if (i >= 1)
+			set_page_private(page, (unsigned long)first_page);
+		if (i >= 2)
+			list_add(&page->lru, &prev_page->lru);
+		if (i == class->pages_per_zspage - 1)	/* last page */
+			SetPagePrivate2(page);
+		prev_page = page;
 	}
 
-	create_page_chain(pages, class->pages_per_zspage);
-	first_page = pages[0];
 	init_zspage(class, first_page);
+
+	error = 0; /* Success */
+
+cleanup:
+	if (unlikely(error) && first_page) {
+		free_zspage(first_page);
+		first_page = NULL;
+	}
 
 	return first_page;
 }
@@ -1434,6 +1421,7 @@ static unsigned long obj_malloc(struct size_class *class,
 	unsigned long m_offset;
 	void *vaddr;
 
+	handle |= OBJ_ALLOCATED_TAG;
 	obj = get_freeobj(first_page);
 	objidx_to_page_and_offset(class, first_page, obj,
 				&m_page, &m_offset);
@@ -1443,10 +1431,10 @@ static unsigned long obj_malloc(struct size_class *class,
 	set_freeobj(first_page, link->next >> OBJ_ALLOCATED_TAG);
 	if (!class->huge)
 		/* record handle in the header of allocated chunk */
-		link->handle = handle | OBJ_ALLOCATED_TAG;
+		link->handle = handle;
 	else
 		/* record handle in first_page->private */
-		set_page_private(first_page, handle | OBJ_ALLOCATED_TAG);
+		set_page_private(first_page, handle);
 	kunmap_atomic(vaddr);
 	mod_zspage_inuse(first_page, 1);
 	zs_stat_inc(class, OBJ_USED, 1);
