7bb1fb99cc32432714dfa2ddd19d604379b2bb1b
Michal Hocko <mhocko@suse.com>
2016-04-07 11:30:47 +0200

    Reverted "zsmalloc: remove page_mapcount_reset"
    This reverts 85256128e59b95f3d01c6a0a49618d00dddb7a8b
    
    Patch has been dropped from mmotm tree with reason: to-by-updated

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -922,6 +922,7 @@ static void reset_page(struct page *page)
 	set_page_private(page, 0);
 	page->mapping = NULL;
 	page->freelist = NULL;
+	page_mapcount_reset(page);
 }
 
 static void free_zspage(struct page *first_page)
