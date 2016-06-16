8c80999d0acbe2d113bd8c54996446fc0acfc371
Michal Hocko <mhocko@suse.com>
2016-06-16 09:19:03 +0200

    Reverted "rmap: support file thp"
    This reverts a27eb1388d787f5b57a2717793bbd3ea68fca74a
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -3319,22 +3319,18 @@ static void __split_huge_page(struct page *page, struct list_head *list)
 
 int total_mapcount(struct page *page)
 {
-	int i, compound, ret;
+	int i, ret;
 
 	VM_BUG_ON_PAGE(PageTail(page), page);
 
 	if (likely(!PageCompound(page)))
 		return atomic_read(&page->_mapcount) + 1;
 
-	compound = compound_mapcount(page);
+	ret = compound_mapcount(page);
 	if (PageHuge(page))
-		return compound;
-	ret = compound;
+		return ret;
 	for (i = 0; i < HPAGE_PMD_NR; i++)
 		ret += atomic_read(&page[i]._mapcount) + 1;
-	/* File pages has compound_mapcount included in _mapcount */
-	if (!PageAnon(page))
-		return ret - compound * HPAGE_PMD_NR;
 	if (PageDoubleMap(page))
 		ret -= HPAGE_PMD_NR;
 	return ret;
