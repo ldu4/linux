9213264116eae22b1e17c49091b54103947b80a9
Michal Hocko <mhocko@suse.com>
2016-06-23 09:44:56 +0200

    Reverted "mm/page_owner: initialize page owner without holding the zone lock"
    This reverts 08d275ea03bff5177226c12158d8e92f1ac5cdb5
    
    Patch has been dropped from mmotm tree with reason: to-beupdated

--- mm/compaction.c
+++ mm/compaction.c
@@ -19,7 +19,6 @@
 #include <linux/kasan.h>
 #include <linux/kthread.h>
 #include <linux/freezer.h>
-#include <linux/page_owner.h>
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -80,8 +79,6 @@ static void map_pages(struct list_head *list)
 		arch_alloc_page(page, order);
 		kernel_map_pages(page, nr_pages, 1);
 		kasan_alloc_pages(page, order);
-
-		set_page_owner(page, order, __GFP_MOVABLE);
 		if (order)
 			split_page(page, order);
 
