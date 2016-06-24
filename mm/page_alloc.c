3fecc69d21234294fe844de6201cbfb2d2f04fa6
Michal Hocko <mhocko@suse.com>
2016-06-24 11:39:09 +0200

    Reverted "mm, vmscan: only wakeup kswapd once per node for the requested classzone"
    This reverts 29cfbccc1cb9327969a7abeb5912c659c8b1f875
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3377,14 +3377,10 @@ static void wake_all_kswapds(unsigned int order, const struct alloc_context *ac)
 {
 	struct zoneref *z;
 	struct zone *zone;
-	pg_data_t *last_pgdat = NULL;
 
 	for_each_zone_zonelist_nodemask(zone, z, ac->zonelist,
-					ac->high_zoneidx, ac->nodemask) {
-		if (last_pgdat != zone->zone_pgdat)
-			wakeup_kswapd(zone, order, ac_classzone_idx(ac));
-		last_pgdat = zone->zone_pgdat;
-	}
+						ac->high_zoneidx, ac->nodemask)
+		wakeup_kswapd(zone, order, ac_classzone_idx(ac));
 }
 
 static inline unsigned int
--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3435,7 +3435,6 @@ kswapd_try_sleep:
 void wakeup_kswapd(struct zone *zone, int order, enum zone_type classzone_idx)
 {
 	pg_data_t *pgdat;
-	int z;
 
 	if (!populated_zone(zone))
 		return;
@@ -3449,16 +3448,8 @@ void wakeup_kswapd(struct zone *zone, int order, enum zone_type classzone_idx)
 	pgdat->kswapd_order = max(pgdat->kswapd_order, order);
 	if (!waitqueue_active(&pgdat->kswapd_wait))
 		return;
-
-	/* Only wake kswapd if all zones are unbalanced */
-	for (z = 0; z <= classzone_idx; z++) {
-		zone = pgdat->node_zones + z;
-		if (!populated_zone(zone))
-			continue;
-
-		if (zone_balanced(zone, order, classzone_idx))
-			return;
-	}
+	if (zone_balanced(zone, order, 0))
+		return;
 
 	trace_mm_vmscan_wakeup_kswapd(pgdat->node_id, zone_idx(zone), order);
 	wake_up_interruptible(&pgdat->kswapd_wait);
