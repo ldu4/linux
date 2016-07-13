a6f3ee87a49e255a0023f43da09908be80f96aaf
Michal Hocko <mhocko@suse.com>
2016-07-13 09:04:57 +0200

    Reverted "mm/page_alloc: introduce post allocation processing on page allocator"
    This reverts b9badceeb848ebb040d6a0600cdfbbb7b08607c2
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- mm/compaction.c
+++ mm/compaction.c
@@ -74,8 +74,14 @@ static void map_pages(struct list_head *list)
 
 		order = page_private(page);
 		nr_pages = 1 << order;
+		set_page_private(page, 0);
+		set_page_refcounted(page);
 
-		post_alloc_hook(page, order, __GFP_MOVABLE);
+		arch_alloc_page(page, order);
+		kernel_map_pages(page, nr_pages, 1);
+		kasan_alloc_pages(page, order);
+
+		set_page_owner(page, order, __GFP_MOVABLE);
 		if (order)
 			split_page(page, order);
 
