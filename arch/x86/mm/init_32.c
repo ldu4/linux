834e870d8c92493de186fea31437537da892ddef
Michal Hocko <mhocko@suse.com>
2017-03-10 09:49:54 +0100

    Reverted "treewide-decouple-cacheflushh-and-set_memoryh-fix"
    This reverts b4f867423567ce1bff9701dde251c06b1478bd78
    
    Patch has been dropped from mmotm tree with reason: to-be-updated

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -46,7 +46,6 @@
 #include <asm/olpc_ofw.h>
 #include <asm/pgalloc.h>
 #include <asm/sections.h>
-#include <asm/set_memory.h>
 #include <asm/paravirt.h>
 #include <asm/setup.h>
 #include <asm/cacheflush.h>
