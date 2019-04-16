a9a7c4656fb92159656e8db48a2a4a4b541fe56f
Laurent Dufour <ldufour@linux.ibm.com>
2019-04-16 15:45:22 +0200

    mm: Add a speculative page fault switch in sysctl
    This allows to turn on/off the use of the speculative page fault handler.
    
    By default it's turned on.
    
    Signed-off-by: Laurent Dufour <ldufour@linux.ibm.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1717,6 +1717,7 @@ extern int fixup_user_fault(struct mm_struct *mm,
 			    bool *unlocked);
 
 #ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+extern int sysctl_speculative_page_fault;
 extern vm_fault_t __handle_speculative_fault(struct mm_struct *mm,
 					     unsigned long address,
 					     unsigned int flags,
@@ -1726,6 +1727,8 @@ static inline vm_fault_t handle_speculative_fault(struct mm_struct *mm,
 						  unsigned int flags,
 						  struct pt_regs *regs)
 {
+	if (unlikely(!sysctl_speculative_page_fault))
+		return VM_FAULT_RETRY;
 	/*
 	 * Try speculative page fault for multithreaded user space task only.
 	 */
