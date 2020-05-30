1d4ba3129c62c110f600a68d3756168057cca4a5
Andrew Morton <akpm@linux-foundation.org>
2020-05-30 02:11:27 +0000

    bpf-bpf_seq_printf-handle-potentially-unsafe-format-string-better-fix
    use strncpy_from_kernel_nofault(), per Christoph
    
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -597,13 +597,8 @@ BPF_CALL_5(bpf_seq_printf, struct seq_file *, m, char *, fmt, u32, fmt_size,
 			}
 
 			unsafe_ptr = (void *)(long)args[fmt_cnt];
-			if ((unsigned long)unsafe_ptr < TASK_SIZE) {
-				err = strncpy_from_user_nofault(
-					bufs->buf[memcpy_cnt], unsafe_ptr,
-					MAX_SEQ_PRINTF_STR_LEN);
-			} else {
-				err = -EFAULT;
-			}
+			err = strncpy_from_kernel_nofault(bufs->buf[memcpy_cnt],
+					unsafe_ptr, MAX_SEQ_PRINTF_STR_LEN);
 			if (err < 0)
 				bufs->buf[memcpy_cnt][0] = '\0';
 			params[fmt_cnt] = (u64)(long)bufs->buf[memcpy_cnt];
